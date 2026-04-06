cfa4a3a4d61dd9d0e3ab2ca8f87544ec19c7a67e
khalid0931 <mutiarif53@gmail.com>
2026-04-06 15:00:48 +0700

    membuat library

---
+++ arraynew.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // deklarasi dengan inisialisasi
+    int aku[5] = {10,20,30,40,50};
+    
+    // deklarasi array tanpa inisialisasi
+    int kamu[5];
+    
+    // tampilkan data
+    cout << "Data pada index 1 = " << aku[1] << endl;
+    
+    // ganti index ke 1
+    aku[1] = 200;
+    cout << endl;
+    cout << "Data pada index 1 = " << aku[1] << endl;
+    
+    // Mengisi Satu per Satu
+    cout << "Data index 0 = ";
+    cin >> kamu[0];
+    
+    cout << "Data index 1 = ";
+    cin >> kamu[1];
+    
+    cout << "Data index 2 = ";
+    cin >> kamu[2];
+    
+    cout << "Data index 3 = ";
+    cin >> kamu[3];
+    
+    cout << "Data index 4 = ";
+    cin >> kamu[4];
+}
